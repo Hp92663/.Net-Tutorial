19cf41037cad769b05b28ed868fb1444ec64f48b
Hp92663 <hirenkhut92663@gmail.com>
2023-10-13 17:31:51 +0530

    add operator & type casting

--- Hello_World/Program.cs
+++ Hello_World/Program.cs
@@ -10,9 +10,103 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Hello Hiren");
-            Console.ReadLine();
+            //Console.WriteLine("Hello World");
+            //Console.WriteLine("Hello");
+            //Console.Write(" Hiren");
+
+            /*
+            // Datatype in C#
+            
+            Integer - int --> 4 bytes
+            Long - long --> 8 bytes
+            Floating Point number - float --> 4 bytes
+            Double  - double --> 8 bytes
+            Character - ch --> 2 bytes
+            String - string --> 2 bytes per characters
+            Boolean - bool -> 'true, false' --> 1 bytes
+
+
+
+            int a = 10;
+            float b = 1.5F;
+            double c = 12.2D;
+            char d = 'a';
+            string e = "hello";
+            bool f = true;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+
+            */
+
+            /*
+            //TypeCasting 
+
+
+            //convert one type to another type.
+            //for Example --
+            // here two types of type casting
+
+             Implicit Casting (automatically) - converting a smaller type to a larger type size
+            char -> int -> long -> float -> double
+
+            Explicit Casting (manually) - converting a larger type to a smaller size type
+            double -> float -> long -> int -> char
+
+            1. Implicit casting
+            
+            2. Explicit casting
+            //convert 'long' to 'int'
+
+            int x = (int) 156.587; 
+            Console.WriteLine(x);
+            
+
+
+           
+            */
+
+            //            /*
+            //Operator
+
+            //Arithmetic Operator
+            double a = 10, b = 20;
+
+            /*
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+           
+
+            // Assignment Operator
+
+            a += b; // a= a + b;
+            a -= b; // a= a - b;
+            a *= b; // a= a * b;
+            a /= b; // a= a / b;
+
+            Console.WriteLine(a);
+  //         */
+
+            // Logical Operator
+
+            Console.WriteLine(true && false);
+            Console.WriteLine(true || false);
+            Console.WriteLine(!true);
+            Console.WriteLine(!false);
+
+            
+            /* 
+             // Maths Function
+
+            */
         }
+
     }
 }
